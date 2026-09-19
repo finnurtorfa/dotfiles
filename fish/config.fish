2269eb7d3303e4cbe7aa14c135541f9f1fdafa7c
Finnur Torfason <finnur.torfason@kivra.com>
2026-09-19 20:02:25 +0000

    fix: added some aliases

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,11 @@ if status is-interactive
     function starship_transient_prompt_func
         starship module character
     end
-    if test "$TERM" != "linux"
+    if test "$TERM" != linux
         starship init fish | source
         enable_transience
     end
-    
+
     # Colors
     if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
         cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
@@ -19,15 +19,19 @@ if status is-interactive
 
     # Aliases
     # kitty doesn't clear properly so we need to do this weird printing
+    alias gst 'git status'
+    alias glg 'git log --oneline --graph --decorate --all'
+    alias g git
+
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias celar "printf '\033[2J\033[3J\033[1;1H'"
     alias claer "printf '\033[2J\033[3J\033[1;1H'"
     alias pamcan pacman
     alias q 'qs -c ii'
-    if test "$TERM" != "linux"
+    if test "$TERM" != linux
         alias ls 'eza --icons=auto'
     end
-    if test "$TERM" = "xterm-kitty"
+    if test "$TERM" = xterm-kitty
         alias ssh 'kitten ssh'
     end
 end
